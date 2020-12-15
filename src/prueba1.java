00d388b407dd9a19affad09c9f0d474c030045d9
JuanLuisHernandezTerron <alum.jluishernandezt@iesalixar.org>
2020-12-15 13:44:47 +0100

    Prueba1

---
+++ src/prueba1.java
@@ -0,0 +1,11 @@
+public class prueba1 {
+
+	public static void main(String[] args) {
+		 int primernumero =  10 ;
+		    int segundonumero =  5 ;
+
+		    System.out.println ( " La división del primernumero y segundonumero es: " );
+		    System.out.println (primernumero / segundonumero);
+	}
+
+}
