8b7a5e6d5591417d2a3d0183bcab9a05eb60b292
María Hernández <Mara Hernndez@MariaHernndez>
2020-12-15 16:14:44 +0100

    ferer

--- src/prueba3.java
+++ src/prueba3.java
@@ -2,7 +2,15 @@
 public class prueba3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 int primernumero= 56;
+	        int segundonumero= 76;
+
+	        System.out.println("El primer numero es:");
+	        System.out.println(primernumero);
+	        System.out.println("Y el seguno numero es:");
+	        System.out.println(segundonumero);
+	        System.out.println("Y la suma es:");
+	        System.out.println(primernumero + segundonumero);
 
 	}
 
